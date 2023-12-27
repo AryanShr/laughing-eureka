d777208c485017bf2f10abd62f5b633aabf93b13
Aryan Shrivastav <aryanshr91003@gmail.com>
2023-12-27 14:05:19 +0530

    cab_booking model

--- src/agents/cab_booking/cab_booking.py
+++ src/agents/cab_booking/cab_booking.py
@@ -1,6 +1,6 @@
 from uagents import Agent, Context, Protocol
 from uagents.setup import fund_agent_if_low
-from messages import Cab, UAgentResponse, UAgentResponseType
+from messages import Cab, UAgentResponse, UAgentResponseType, CabSelection
 import requests
 import os
 import uuid
@@ -16,33 +16,39 @@ fund_agent_if_low(agent.wallet.address())
 @agent.on_event("startup")
 async def startup(ctx: Context):
     ctx.logger.info("Cab booking agent started")
-    await ctx.experimental_broadcast("CabBooking",Cab(10,20))
+    await ctx.experimental_broadcast("CabBooking",Cab(distance_from_source=10, time_to_destination=20))
 
-cab_protocol = Protocol("CabBooking")
-
-@cab_protocol.on_message(model=Cab, replies={UAgentResponse})
-async def get_cab(ctx: Context, sender: str, msg: Cab):
+@agent.on_message(model=CabSelection)
+async def cab_selection(ctx: Context, sender: str, msg: CabSelection):
     ctx.logger.info(f"Received message from {sender}, session: {ctx.session}")
-    # This is just example for 5 cab options
-
-    cab_options = [
-        {"name": "Uber", "price": 100, "time": 10},
-        {"name": "Ola", "price": 120, "time": 15},
-        {"name": "Lyft", "price": 110, "time": 12},
-        {"name": "Bolt", "price": 90, "time": 8},
-        {"name": "Juno", "price": 95, "time": 9},
-    ]
-
-    await ctx.send(
-        sender,
-        UAgentResponse(
-            options=cab_options,
-            type=UAgentResponseType.SELECT_FROM_OPTIONS
-        )
-    )
+    ctx.logger.info(f"{msg}")
 
 
-agent.include(cab_protocol)
+# cab_protocol = Protocol("CabBooking")
+
+# @cab_protocol.on_message(model=Cab, replies={UAgentResponse})
+# async def get_cab(ctx: Context, sender: str, msg: Cab):
+#     ctx.logger.info(f"Received message from {sender}, session: {ctx.session}")
+#     # This is just example for 5 cab options
+
+#     cab_options = [
+#         {"name": "Uber", "price": 100, "time": 10},
+#         {"name": "Ola", "price": 120, "time": 15},
+#         {"name": "Lyft", "price": 110, "time": 12},
+#         {"name": "Bolt", "price": 90, "time": 8},
+#         {"name": "Juno", "price": 95, "time": 9},
+#     ]
+
+#     await ctx.send(
+#         sender,
+#         UAgentResponse(
+#             options=cab_options,
+#             type=UAgentResponseType.SELECT_FROM_OPTIONS
+#         )
+#     )
+
+
+# agent.include(cab_protocol)
 
 if __name__ == "__main__":
     agent.run()
--- src/messages/cab.py
+++ src/messages/cab.py
@@ -6,4 +6,8 @@ from pydantic import Field
 class Cab(Model):
     distance_from_source: int = Field(description="The distance from the source location to the destination location.")
     distance_for_travel: int = Field(description="The distance that the user is willing to travel to reach the destination.")
-    
+
+class CabSelection(Model):
+    is_available: bool = Field(description="The field expresses whether the cab is available or not.")
+    fare: int = Field(description="The fare of the cab.")
+    arrival_time: int = Field(description="The time taken by the cab to reach the user.")
