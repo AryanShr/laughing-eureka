5fc93005bf4c5303febe9ff101e9a82718e726dc
Omkar Borker <omkarborker15@gmail.com>
2023-12-27 16:40:53 +0530

    changes

--- src/messages/cab.py
+++ src/messages/cab.py
@@ -4,6 +4,8 @@ from typing import Optional, List
 from pydantic import Field
 
 class Cab(Model):
+    source : str = Field(description="the location of the starting point")
+    destination: str = Field(description="the location of the ending of journey")
     distance_from_source: int = Field(description="The distance from the source location to the destination location.")
     distance_for_travel: int = Field(description="The distance that the user is willing to travel to reach the destination.")
 
