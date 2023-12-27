ad7e9ba83f139425d28a4cf947a6136021460afd
Omkar Borker <omkarborker15@gmail.com>
2023-12-27 19:52:41 +0530

    distance calc

--- src/utils/distCal.py
+++ src/utils/distCal.py
@@ -1,42 +1,23 @@
 import requests
-import math    
-api_key = 'AIzaSyDiB8cANSb9SnlY94TEA-y7NvLWOW3_Jdw'
-# Change address format
-# addr_from = address_from.replace(' ', '+')
-# formatted_addr_to = address_to.replace(' ', '+')
-# Geocoding API request with start address
-geocode_from = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address=panvel&sensor=false&key=AIzaSyDiB8cANSb9SnlY94TEA-y7NvLWOW3_Jdw').json()
-if 'error_message' in geocode_from:
-    print(geocode_from['error_message'])
-if 'results' not in geocode_from or len(geocode_from['results']) == 0:
-    print('No geocoding results found for the starting address')
 
-# Geocoding API request with end address
-geocode_to = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address=powai&sensor=false&key=AIzaSyDiB8cANSb9SnlY94TEA-y7NvLWOW3_Jdw').json()
-if 'error_message' in geocode_to:
-    print(geocode_to['error_message'])
+url = "https://distanceto.p.rapidapi.com/distance/route"
 
-if 'results' not in geocode_to or len(geocode_to['results']) == 0:
-    print('No geocoding results found for the destination address')
+payload = { "route": [
+		{
+			"country": "IND",
+			"name": "Margao"
+		},
+		{
+			"country": "IND",
+			"name": "Panjim"
+		}
+	] }
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "34d900abadmsh45f8cb12323ccd9p1dd0d4jsnbf35c367191a",
+	"X-RapidAPI-Host": "distanceto.p.rapidapi.com"
+}
 
-# Get latitude and longitude from the geodata
-latitude_from = geocode_from['results'][0]['geometry']['location']['lat']
-longitude_from = geocode_from['results'][0]['geometry']['location']['lng']
-latitude_to = geocode_to['results'][0]['geometry']['location']['lat']
-longitude_to = geocode_to['results'][0]['geometry']['location']['lng']
+response = requests.post(url, json=payload, headers=headers)
 
-# Calculate distance between latitude and longitude
-theta = longitude_from - longitude_to
-dist = math.sin(math.radians(latitude_from)) * math.sin(math.radians(latitude_to)) + math.cos(math.radians(latitude_from)) * math.cos(math.radians(latitude_to)) * math.cos(math.radians(theta))
-dist = math.acos(dist)
-dist = math.degrees(dist)
-miles = dist * 60 * 1.1515
-
-# Convert unit and print(distance
-unit = 'K'
-if unit == 'K':
-    print(round(miles * 1.609344, 2))
-elif unit == 'M':
-    print(round(miles * 1609.344, 2))
-else:
-    print(round(miles, 2))
+print(response.json())
