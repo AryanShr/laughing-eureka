1801790ebe2bd62c1202056317a4afd0811e59c3
Omkar Borker <omkarborker15@gmail.com>
2023-12-27 21:02:47 +0530

    changes to dist calc

--- src/utils/distCal.py
+++ src/utils/distCal.py
@@ -1,23 +1,34 @@
 import requests
 
-url = "https://distanceto.p.rapidapi.com/distance/route"
+def get_distance(location1, location2):
+    url = "https://distanceto.p.rapidapi.com/distance/route"
 
-payload = { "route": [
-		{
-			"country": "IND",
-			"name": "Margao"
-		},
-		{
-			"country": "IND",
-			"name": "Panjim"
-		}
-	] }
-headers = {
-	"content-type": "application/json",
-	"X-RapidAPI-Key": "34d900abadmsh45f8cb12323ccd9p1dd0d4jsnbf35c367191a",
-	"X-RapidAPI-Host": "distanceto.p.rapidapi.com"
-}
+    payload = {
+        "route": [
+            {
+                "country": "IND",
+                "name": location1
+            },
+            {
+                "country": "IND",
+                "name": location2
+            }
+        ]
+    }
+    
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "34d900abadmsh45f8cb12323ccd9p1dd0d4jsnbf35c367191a",
+        "X-RapidAPI-Host": "distanceto.p.rapidapi.com"
+    }
 
-response = requests.post(url, json=payload, headers=headers)
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data['route']['car']['distance']
+    except requests.exceptions.RequestException as e:
+        return {"error": str(e)}
 
-print(response.json())
+result = get_distance("Margao", "Panjim")
+print(result)
